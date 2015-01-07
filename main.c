7f45495aaeb02ed0a2da9be733a2c6dddb50711c
MrOnak <dst@st-webdevelopment.com>
2015-01-07 20:45:19 +0100

    implemented a full-blown serial client that samples data only after being told so to reduce load on Java serial client.

--- main.c
+++ main.c
@@ -25,9 +25,16 @@ int main() {
   init();
 
   while (1) {
-    uart_putc(PINB);
-
-    _delay_us(1);
+    commandParser();
+    
+    if (running == 1) {
+      if (triggered == 0 && noTrigger == 0) {
+        checkTriggers();
+        lastData = PINB;
+      } else {
+        writeToBuffer();
+      }
+    }
   }
 
   return 0;
@@ -44,5 +51,251 @@ void init() {
     DDRD  &= ~(1 << PB0) | ~(1 << PB1) | ~(1 << PB2) | ~(1 << PB3) | ~(1 << PB4) | ~(1 << PB5) | ~(1 << PB6) | ~(1 << PB7);
     PORTB |=  (1 << PB0) |  (1 << PB1) |  (1 << PB2) |  (1 << PB3) |  (1 << PB4) |  (1 << PB5) |  (1 << PB6) |  (1 << PB7);
     
-    uart_puts_P("8 channel Logic Analyzer ready");
+    // set all flags
+    running = 0;
+    triggers[0] = 0x00;
+    triggers[1] = 0x00;
+    noTrigger = 1;
+    triggered = 0;
+    lastData = 0x00;
+    
+    //uart_puts_P("8 channel Logic Analyzer ready");
+    
+}
+
+/**
+ * reads commands from UART and calls the appropriate subroutine
+ */
+void commandParser() {
+  if (uart_available() > 0) {
+    uint16_t c = uart_getc();
+    uint16_t c2;
+    
+    if ( c & UART_NO_DATA ) {
+      // no data available from UART 
+    } else {
+      // new data available from UART - check for Frame or Overrun error
+      if ( c & UART_FRAME_ERROR ) {
+        // Framing Error detected, i.e no stop bit detected
+        uart_puts_P("UART Frame Error: ");
+      }
+      
+      if ( c & UART_OVERRUN_ERROR ) {
+        // Overrun, a character already present in the UART UDR register was not read by the interrupt handler 
+        // before the next character arrived, one or more received characters have been dropped
+        uart_puts_P("UART Overrun Error: ");
+      }
+      if ( c & UART_BUFFER_OVERFLOW ) {
+        //We are not reading the receive buffer fast enough, one or more received character have been dropped 
+        uart_puts_P("Buffer overflow error: ");
+      }
+      
+      switch (c) {
+        case CMD_SEND_MAX_BUFFER_SIZE:  // 'm'
+          cmdSendMaxBufferSize();
+          break;
+        case CMD_SEND_BUFFER_SIZE:      // 'b'
+          cmdSendBufferSize();
+          break;
+        case CMD_SET_BUFFER_SIZE:       // 'M'
+          while (uart_available() == 0) {}
+          c = uart_getc();
+          if ((c >> 8) == 0) {
+            cmdSetBufferSize(c);
+          } else {
+            sendNack();
+          }
+          break;
+        case CMD_SET_TRIGGER:           // 'T'
+          while (uart_available() < 2) {}
+          c = uart_getc();
+          c2 = uart_getc();
+          if ((c >> 8) == 0 && (c2 >> 8) == 0) {
+            cmdSetTrigger(c, c2);
+          } else {
+            sendNack();
+          }
+          break;
+        case CMD_GET_TRIGGER:           // 't'
+          cmdGetTrigger();
+          break;
+        case CMD_NO_TRIGGER:            // 'N'
+          cmdNoTrigger();
+          break;
+        case CMD_START:                 // 'S'
+          cmdStart();
+          break;
+        case CMD_STOP:                  // 's'
+          cmdStop();
+          break;
+        case CMD_SEND_BUFFER:           // 'B'
+          cmdSendBuffer();
+          break;
+        default:
+          sendErr(ERR_INVALID_COMMAND);
+      }
+    }    
+  }
+}
+
+/**
+ * sends the maximum allowed buffer size to the client
+ */
+void cmdSendMaxBufferSize() {
+  sendAck();
+  uart_putc(MAX_BUFFER_SIZE);
+}
+
+/**
+ * sends the current buffer size to the client
+ */
+void cmdSendBufferSize() {
+  sendAck();
+  uart_putc(bufferSize);
+}
+
+/**
+ * sets the ring buffer to the given size
+ */
+void cmdSetBufferSize(uint8_t size) {
+  if (size <= MAX_BUFFER_SIZE) {
+    bufferSize = size;
+    sendAck();
+  } else {
+    sendErr(ERR_BUFFER_TOO_LARGE);
+  }    
+}
+
+/**
+ * configures the input triggers and sets
+ * the noTrigger flag to 0.
+ */
+void cmdSetTrigger(uint8_t rising, uint8_t falling) {
+  noTrigger = 0;
+  triggers[0] = rising;
+  triggers[1] = falling;
+  sendAck();
+}
+
+ /**
+  * erases input triggers and sets the noTrigger flag
+  *
+  * this will cause data gathering to immediately start
+  * after cmdStart() is being issued.
+  */
+ void cmdNoTrigger() {
+   noTrigger = 1;
+   triggers[0] = 0x00;
+   triggers[1] = 0x00;
+   sendAck();
+}
+
+/**
+ * sends the rising and falling trigger bitmasks (in that order)
+ */
+void cmdGetTrigger() {
+  uart_putc(triggers[0]);
+  uart_putc(triggers[1]);  
+}
+ 
+ 
+/**
+ * starts input gathering. also resets the buffer position
+ * to zero, effectively erasing the buffer as it's being read later
+ */
+void cmdStart() {
+  uint8_t i;
+  
+  if (running == 0) {
+    bufferPos = 0;
+    running = 1;
+    triggered = 0;
+    lastData = PINB;
+    
+    for (i = 0; i < MAX_BUFFER_SIZE; i++) {
+      buffer[i] = 0x00;
+    }
+    
+    sendAck();
+  } else {
+    sendErr(ERR_RUNNING);
+  }
+}
+
+/**
+ * stops the data gathering
+ *
+ * this is useful in case the triggers don't get triggered.
+ *
+ * will set running to 0
+ */
+void cmdStop() {
+  running = 0;
+  sendAck();
+}
+
+/** 
+ * sends the recorded data buffer to the client
+ */
+void cmdSendBuffer() {
+  uint8_t i;
+  
+  if (running == 0) {
+    for (i = 0; i < bufferSize; i++) {
+      uart_putc(buffer[i]);
+    }
+  } else {
+    sendErr(ERR_RUNNING);
+  }
+}
+
+/**
+ * sends an acknowledgement byte
+ */
+void sendAck() {
+  uart_putc(ACK);
+}
+
+/**
+ * sends a generic not-acknowledged byte
+ */
+void sendNack() {
+  uart_putc(NACK);
+}
+
+/**
+ * sends an error
+ */
+void sendErr(uint16_t err) {
+  uart_putc(err >> 8);
+  uart_putc(err);
+}
+
+/**
+ * checks the data register against trigger conditions
+ * and resets the triggered flag if they match
+ */ 
+void checkTriggers() {
+  // @todo implement checkTriggers()
+}
+
+/**
+ * writes the current state of PINB into the ringbuffer
+ * and advances the position.
+ *
+ * If the position reaches the bufferSize then reading is
+ * terminated.
+ *
+ * once reading is finished an ACK will be sent
+ */
+void writeToBuffer() {
+  buffer[bufferPos] = PINB;
+  bufferPos++;
+  
+  // stop reading if buffer size is reached
+  if (bufferPos == bufferSize) {
+    running   = 0;
+    triggered = 0;
+    uart_putc(READY);
+  }
 }
--- main.h
+++ main.h
@@ -6,6 +6,175 @@
 #define F_CPU 8000000UL
 #endif
 
+#define NACK 0x30                         // '0'
+#define ACK 0x31                          // '1'
+#define READY 0x32                        // '2' - send after data gathering is completed
+
+#define ERR_BUFFER_TOO_LARGE 0x4531       // 'E1'
+#define ERR_INVALID_COMMAND 0x4532        // 'E2'
+#define ERR_RUNNING 0x4533                // 'E3' - send when commands are sent while gathering is running
+
+#define CMD_SEND_MAX_BUFFER_SIZE 0x6D     // 'm'
+#define CMD_SEND_BUFFER_SIZE 0x62         // 'b'
+#define CMD_SET_BUFFER_SIZE 0x4D          // 'M'
+#define CMD_SET_TRIGGER 0x54              // 'T'
+#define CMD_GET_TRIGGER 0x74              // 't'
+#define CMD_NO_TRIGGER 0x4E               // 'N'
+#define CMD_START 0x53                    // 'S'
+#define CMD_STOP 0x73                     // 's'
+#define CMD_SEND_BUFFER 0x42              // 'B'
+
+#define MAX_BUFFER_SIZE 40
+
+/**
+ * contains the register value from last iteration
+ *
+ * is used by checkTriggers()
+ */
+uint8_t lastData;
+
+/**
+ * configured buffer size.
+ *
+ * note that the actual buffer is always of MAX_BUFFER_SIZE
+ * size but reading data will stop once bufferPos matches
+ * bufferSize
+ */
+uint8_t bufferSize = MAX_BUFFER_SIZE;
+
+/**
+ * the data buffer
+ */
+uint8_t buffer[MAX_BUFFER_SIZE];
+/**
+ * current position in the buffer
+ */
+uint8_t bufferPos = 0;
+
+/** 
+ * rising and falling triggers (in that order)
+ */
+uint8_t triggers[2] = {0x00, 0x00};
+
+/**
+ * flag that indicates that data gathering is currently in progress
+ * when set to 1.
+ */
+uint8_t running = 0;
+ 
+/**
+ * set 1 after the trigger conditions are met. 
+ * writeToBuffer() will not fill the buffer until
+ * this is 1 OR noTrigger is 1
+ */
+uint8_t triggered = 0;
+
+/**
+ * if set 1, then writeToBuffer() will immediately
+ * start filling the buffer after cmdStart() has been
+ * called.
+ *
+ * if it is 0 then writeToBuffer() requires triggered to 
+ * be 1.
+ */
+uint8_t noTrigger = 1;
+
+/**
+ * Initializes the PB register as pullup-inputs and starts UART
+ */
 void init();
+
+/**
+ * reads commands from UART and calls the appropriate subroutine
+ */
+void commandParser();
+
+/**
+ * sends the maximum allowed buffer size to the client
+ */
+void cmdSendMaxBufferSize();
+
+/**
+ * sends the current buffer size to the client
+ */
+void cmdSendBufferSize();
+
+/**
+ * sets the ring buffer to the given size
+ */
+void cmdSetBufferSize(uint8_t size);
+
+/**
+ * configures the input triggers and sets
+ * the noTrigger flag to 0.
+ */
+void cmdSetTrigger(uint8_t rising, uint8_t falling);
+
+ /**
+  * erases input triggers and sets the noTrigger flag
+  *
+  * this will cause data gathering to immediately start
+  * after cmdStart() is being issued.
+  */
+void cmdNoTrigger();
+
+/**
+ * sends the rising and falling trigger bitmasks (in that order)
+ */
+void cmdGetTrigger();
+ 
+/**
+ * starts input gathering. 
+ *
+ * also resets the buffer position
+ * to zero, effectively erasing the buffer as it's been read
+ */
+void cmdStart();
+
+/**
+ * stops the data gathering
+ *
+ * this is useful in case the triggers don't get triggered.
+ *
+ * will set running to 0
+ */
+void cmdStop();
+
+/** 
+ * sends the recorded data buffer to the client
+ */
+void cmdSendBuffer();
+
+/**
+ * sends an acknowledgement byte
+ */
+void sendAck();
+
+/**
+ * sends a generic not-acknowledged byte
+ */
+void sendNack();
+ 
+/**
+ * sends an error
+ */
+void sendErr(uint16_t err);
+
+/**
+ * checks the data register against trigger conditions
+ * and resets the triggered flag if they match
+ */
+void checkTriggers();
+
+/**
+ * writes the current state of PINB into the ringbuffer
+ * and advances the position.
+ *
+ * If the position reaches the bufferSize then reading is
+ * terminated.
+ *
+ * once reading is finished an ACK will be sent
+ */
+void writeToBuffer();
 
 #endif // LOGIC_ANALYZER_H
